a4d25787ffff43b9dee6305bbbc115d6b03dfbf6
CoGrammar Code Review <reviews@cogrammar.com>
2019-08-08 08:37:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -46,4 +46,14 @@ def update_counting_hash(hash, key)
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
 
+  if hash.has_key?(key)
+    hash[key] += 1
+    puts hash
+    puts hash.keys
+  else
+    hash[key] = 1
+    puts hash
+    puts hash.keys
+  end
 end
+update_counting_hash({},'hello')
