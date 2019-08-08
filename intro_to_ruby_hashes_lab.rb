1f10931f84899a489c777112d4e3a7c8bac765c6
CoGrammar Code Review <reviews@cogrammar.com>
2019-08-08 08:48:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,9 +39,7 @@ my_hash_creator(:name, "Najraah")
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  hash_new = {
-
-  }
+  hash_new
 end
 read_from_hash( {:name => 'JP',:age => 23}, :name)
 
