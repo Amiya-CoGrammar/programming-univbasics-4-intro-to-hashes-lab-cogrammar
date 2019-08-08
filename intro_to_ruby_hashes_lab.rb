0baf6bc825dbbcdeb5503d692f141d3f83099518
CoGrammar Code Review <reviews@cogrammar.com>
2019-08-08 08:44:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,7 +35,7 @@ def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   hash_new = {}
 end
-my_hash_creator(:name, Najraah)
+my_hash_creator(:name, "Najraah")
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
