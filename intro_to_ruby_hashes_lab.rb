acc80a53672fa6531331d745a9638c1512115628
CoGrammar Code Review <reviews@cogrammar.com>
2019-08-08 08:33:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,17 +1,36 @@
 def new_hash
   # return an empty hash
+  new_hash = {}
+
+  return new_hash
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+
+  my_hash = {
+    :name => "Johnny",
+    :age => 23
+  }
+  return my_hash
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {
+    :name => "Grace Hopper"
+  }
+  return pioneer
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+
+  hash_id = { :key1 => 'value1',
+        :key2 => 'value2',
+        :key3 => 'value3'
+      }
+  returns hash_id
 end
 
 def my_hash_creator(key, value)
@@ -26,4 +45,5 @@ def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
+
 end
